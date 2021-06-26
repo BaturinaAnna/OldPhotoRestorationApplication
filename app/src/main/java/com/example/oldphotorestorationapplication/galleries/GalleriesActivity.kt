8e7b1b092618a901045ba86df47f50c721181d36
Baturina Anna <baturinaanya@yandex.ru>
2021-06-26 14:31:55 +0300

    solved conflicts

--- app/src/main/java/com/example/oldphotorestorationapplication/galleries/GalleriesActivity.kt
+++ app/src/main/java/com/example/oldphotorestorationapplication/galleries/GalleriesActivity.kt
@@ -5,7 +5,6 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
